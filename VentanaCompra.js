6f27d520217f48cd147b249b8883ab925b78490a
kevin2005631 <kevinjaramillodiaz8@gmail.com>
2025-11-10 23:27:38 -0600

    Add files via upload

--- VentanaCompra.js
+++ VentanaCompra.js
@@ -1,18 +1,15 @@
-
 document.addEventListener("DOMContentLoaded", function() {
   const form = document.querySelector("form");
   const popup = document.getElementById("popup");
   const cerrarPopup = document.getElementById("cerrarPopup");
 
-
   form.addEventListener("submit", function(e) {
-    e.preventDefault(); 
-    popup.style.display = "block"; 
+    e.preventDefault();
+    popup.style.display = "block";
   });
 
-
   cerrarPopup.addEventListener("click", function() {
-    popup.style.display = "none"; 
+    popup.style.display = "none";
     form.reset();
   });
 });
